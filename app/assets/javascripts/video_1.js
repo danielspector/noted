cc844562bf5e1d49c12ca7d000a668b26e5e743c
sarony <saron.yitbarek@gmail.com>
2013-11-19 14:40:39 -0500

    updated some files..

--- app/assets/javascripts/video_1.js
+++ app/assets/javascripts/video_1.js
@@ -1,16 +1,17 @@
 $(document).ready(function(){
-  var myPlayer = new MediaElementPlayer("#lecture_video");
+  
+  $("#new_note").click(function(e){
+    e.preventDefault();
 
-  $("#new_note").click(function(){
-    // var timeStamp = myPlayer.currentTime;
-      // alert(timeStamp);
-    myPlayer.pause();
-    var timeStamp = $("#lecture_video").currentTime();
-    // var timeStamp = myPlayer.currentTime();
-    console.log(timeStamp);
+    var $myPlayer = $("#lecture_video");
+    $myPlayer[0].pause();
+    var timeStamp = $myPlayer[0].currentTime;
+    $("#note_video_timestamp").val(timeStamp)
+
     // $.post('/videos/:id/notes', {video_time: timeStamp}, function(response){
     //     //COME BACK FOR TIMESTAMP FUNCTIONALITY
     // });
+
   });
 
 });
