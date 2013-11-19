ec1123766cda374d40be897da7d2dd3efa996786
sarony <saron.yitbarek@gmail.com>
2013-11-19 09:25:03 -0500

    pushed

---
+++ app/assets/javascripts/video_1.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  alert("Please fetch my silver spoon");
+  var myPlayer = _V_("#lecture_video");
+  //var timeStamp = myPlayer.currentTime();
+  //myPlayer.pause();
+  $("#new_note").click(function(){
+    myPlayer.pause();
+    var timeStamp = myPlayer.currentTime();
+    // $.post('/videos/:id/notes', {video_time: timeStamp}, function(response){
+    //     //COME BACK FOR TIMESTAMP FUNCTIONALITY
+    // });
+  });
+
+
+});
