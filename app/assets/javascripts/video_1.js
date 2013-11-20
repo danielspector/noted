6ec268b9d07cff1edd9873b0d5c57dddaf0d882d
stephaniehoh <stephoh@gmail.com>
2013-11-20 15:15:52 -0500

    minor changes to video_1.js file

--- app/assets/javascripts/video_1.js
+++ app/assets/javascripts/video_1.js
@@ -8,16 +8,16 @@ $(document).ready(function(){
     var timeStamp = $myPlayer[0].currentTime;
     $("#note_video_timestamp").val(timeStamp);
 
-  $("#new_note").click(function(){
-    // var timeStamp = myPlayer.currentTime;
-      // alert(timeStamp);
-    myPlayer.pause();
-    var timeStamp = $("#lecture_video").currentTime ();
-    // var timeStamp = myPlayer.currentTime();
-    console.log(timeStamp);
-    // $.post('/videos/:id/notes', {video_time: timeStamp}, function(response){
-    //     //COME BACK FOR TIMESTAMP FUNCTIONALITY
-    // });
+  // $("#new_note").click(function(){
+  //   // var timeStamp = myPlayer.currentTime;
+  //     // alert(timeStamp);
+  //   myPlayer.pause();
+  //   var timeStamp = $("#lecture_video").currentTime ();
+  //   // var timeStamp = myPlayer.currentTime();
+  //   console.log(timeStamp);
+  //   // $.post('/videos/:id/notes', {video_time: timeStamp}, function(response){
+  //   //     //COME BACK FOR TIMESTAMP FUNCTIONALITY
+  //   // });
   });
 
   $("input[value='Create Note']").click(function(e){
