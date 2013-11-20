d5e1991565a49ed788514e9fdbc5b5101e06141f
irmiller22 <irmiller22@gmail.com>
2013-11-20 13:24:29 -0500

    set up CRUD app actions for videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,33 +1,46 @@
 class VideosController < ApplicationController
-
+  before_action :set_video, :only => [:show, :edit, :update, :destroy]
   def index
+    @video = Video.new
     @videos = Video.all
   end
 
   def new
+    @video = Video.new
   end
 
   def create
+    @video = Video.new(video_params)
+    @video.save
+    redirect_to videos_path
   end
 
   def show
-    @video = Video.find_by(id: params[:video_id])
     @note = Note.new
   end
 
   def edit
+
   end
 
   def update
+    @video.update(video_params)
+    redirect_to(@video)
   end
 
   def destroy
+    @video.destroy
+    redirect_to videos_path
   end
 
   private
 
+  def set_video
+    @video = Video.find_by(:id => params[:video_id])
+  end
+
   def video_params
-    params.require(:video).permit(:id, :name)
+    params.require(:video).permit(:name, :link, :lecture_date)
   end
 
 end
