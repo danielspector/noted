de969c556aa880293c1bd64980d14017a4d13797
sarony <saron.yitbarek@gmail.com>
2013-11-19 19:28:36 -0500

    some mad progress

--- app/assets/javascripts/video_1.js
+++ app/assets/javascripts/video_1.js
@@ -10,4 +10,22 @@ $(document).ready(function(){
 
   });
 
+  $("input[value='Create Note']").click(function(e){
+    e.preventDefault();
+
+    var note_form = 
+    var $myPlayer = $("#lecture_video");
+    $myPlayer[0].play();
+    var note_timestamp = $('.new_note #note_video_timestamp').val();
+    var note_body = $('.new_note #note_body').val();
+    var data = { note_video_timestamp: note_timestamp, note_body: note_body};
+    var video_id = $("#video_id").val();
+    $.post("/videos/"+video_id+"/notes", data, function(note){
+        $('.append_note').append('<li>'+note.video_timestamp+'</li><br><li>'+note.body+'</li>');
+    });
+
+  });
+
 });
+
+// $( "input[value='Hot Fuzz']" ).next().text( "Hot Fuzz" );
