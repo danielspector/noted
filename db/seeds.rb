be36fd0044974a09b78eac84717a1d4840f59c13
sarony <saron.yitbarek@gmail.com>
2013-11-20 17:07:48 -0500

    login and authentication works

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-avi = User.create([{name: "Avi", email: "avi@flombaum.com", password_digest: "itest", permission_type: "instructor"}])
+avi = User.create([{name: "Avi", email: "avi@flombaum.com", password: "itest", permission_type: "instructor"}])
 
 videos = Video.create([{instructor_id: 1, name: "Lecture 1", link: "http://flatiron-school.s3.amazonaws.com/ruby-003/reviews/associations.and.forms.mp4", lecture_date: "11/02/2013"}, {name: "Lecture 2", instructor_id: 1, link: "http://flatiron-school.s3.amazonaws.com/ruby-003/reviews/playister-associations-with-artists.mp4", lecture_date: "11/10/2013"} ])
 
+ian = User.create({name: "Ian", email: "ian@miller.com", password: "stest", permission_type: "student"})
 
-
-ian = User.create({name: "Ian", email: "ian@miller.com", password_digest: "stest", permission_type: "student"})
-
-stephanie = User.create({name: "Stephanie", email: "stephanie@oh.com", password_digest: "stest1", permission_type: "student"})
+stephanie = User.create({name: "Stephanie", email: "stephanie@oh.com", password: "stest1", permission_type: "student"})
