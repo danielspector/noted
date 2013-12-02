9aedd5c6d64371e04491809e95ec2e1b30d57cbb
sarony <saron.yitbarek@gmail.com>
2013-12-02 15:49:27 -0500

    video hover on show fixed

--- app/assets/javascripts/video_1.js
+++ app/assets/javascripts/video_1.js
@@ -225,7 +225,7 @@ $(document).ready(function(){
 
                   
   // INDEX - HIDE AND SHOW TOGGLE
-  $('video').hover(function(){
+  $('.video_wrap video').hover(function(){
     $(this).closest('.video-js-box').fadeTo(200, 0);
   }, function(){
     $(this).closest('.video-js-box').fadeTo(200, 1);
