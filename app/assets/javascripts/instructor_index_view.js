b5d98fcf65b25535cf95fe0a30af052b212dc4d8
irmiller22 <irmiller22@gmail.com>
2013-12-03 08:33:52 -0500

    fancybox attempt #1

--- app/assets/javascripts/instructor_index_view.js
+++ app/assets/javascripts/instructor_index_view.js
@@ -1,8 +1,29 @@
 // FORM OVERLAY FOR INSTRUCTOR - NEW VIDEO FEATURE
 
-// $(document).ready(function(){
-// 	$(".nav-new-video").on("click", function(){
-// 		$.get
-// 	} )
+$(document).ready(function(){
+	$(".nav-new-video a").fancybox();
 
-// });
+	// $('.nav-right-options').on("click", ".nav-new-video a", function(e){
+	// 	e.preventDefault();
+	// 	alert("this works");
+
+		// $.get("/videos/new", function(data){
+		// 	// $('.js-new-video').append(data);
+
+		// 	$('#video_all').hide();
+		});
+
+		var name = $("#video_name").val();
+		var description = $("#video_description").val();
+		var video_link = $("#video_link").val();
+		var video_lecture_date = $("#video_lecture_date").val();
+		data = {
+			name: name,
+			description: description,
+			video_link: video_link,
+			video_lecture_date
+		}
+
+	});
+
+});
