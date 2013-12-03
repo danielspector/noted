4f3bfd8e3c5731063de99a3f23122ae33bc04948
irmiller22 <irmiller22@gmail.com>
2013-12-02 21:35:45 -0500

    ajax for sign-up form, fixed nav-bar for instructor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 	skip_before_action :login_required, :only => [:new, :create]
 	def new
 		@user = User.new
+		render :partial => 'users/form', :format => 'text/html'
 	end
 
 	def create
@@ -9,10 +10,12 @@ class UsersController < ApplicationController
 		@user.permission_type = "student"
 		if @user.save
 			login(@user.id)
+			redirect_to login_path
 		else
 			flash[:error] = "We were not able to process your request!"
 			render :new
 		end
+		
 	end
 
 	private
