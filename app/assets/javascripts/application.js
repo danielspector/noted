fe8d9385392ed611edf24b3dcbbbadd671b84e60
irmiller22 <irmiller22@gmail.com>
2013-12-03 09:21:16 -0500

    css modal implemented for video form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require fancybox
+//= require twitter/bootstrap/modal
 //= require jquery_ujs
 
 //= require video
--- app/assets/javascripts/instructor_index_view.js
+++ app/assets/javascripts/instructor_index_view.js
@@ -1,7 +1,11 @@
 // FORM OVERLAY FOR INSTRUCTOR - NEW VIDEO FEATURE
 
 $(document).ready(function(){
-	$(".nav-new-video a").fancybox();
+	$(".nav-new-video a").click(function(e){
+		e.preventDefault();
+
+		$.getScript("/videos/new");
+	});
 
 	// $('.nav-right-options').on("click", ".nav-new-video a", function(e){
 	// 	e.preventDefault();
@@ -11,19 +15,19 @@ $(document).ready(function(){
 		// 	// $('.js-new-video').append(data);
 
 		// 	$('#video_all').hide();
-		});
+		// });
 
-		var name = $("#video_name").val();
-		var description = $("#video_description").val();
-		var video_link = $("#video_link").val();
-		var video_lecture_date = $("#video_lecture_date").val();
-		data = {
-			name: name,
-			description: description,
-			video_link: video_link,
-			video_lecture_date
-		}
+		// var name = $("#video_name").val();
+		// var description = $("#video_description").val();
+		// var video_link = $("#video_link").val();
+		// var video_lecture_date = $("#video_lecture_date").val();
+		// data = {
+		// 	name: name,
+		// 	description: description,
+		// 	video_link: video_link,
+		// 	video_lecture_date
+		// }
 
-	});
+	// });
 
 });
