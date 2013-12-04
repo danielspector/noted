27d8a9f2e20d5f4449fc1131f0256802a6f9c8b3
irmiller22 <irmiller22@gmail.com>
2013-12-04 09:45:54 -0500

    added video_js font files to app/assets folder

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ FlatironVideos::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Video.js configuration
   config.assets.precompile += %w( video-js.swf vjs.eot vjs.svg vjs.ttf vjs.woff )
