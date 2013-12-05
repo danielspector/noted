973d135ec181d1438b09bb0febc895e7a6ed9dfd
sarony <saron.yitbarek@gmail.com>
2013-12-04 23:12:32 -0500

    gradient and button fix

--- app/assets/javascripts/instructor_index_view.js
+++ app/assets/javascripts/instructor_index_view.js
@@ -14,27 +14,4 @@ $(document).ready(function(){
 
 	});
 
-	// $('.nav-right-options').on("click", ".nav-new-video a", function(e){
-	// 	e.preventDefault();
-	// 	alert("this works");
-
-		// $.get("/videos/new", function(data){
-		// 	// $('.js-new-video').append(data);
-
-		// 	$('#video_all').hide();
-		// });
-
-		// var name = $("#video_name").val();
-		// var description = $("#video_description").val();
-		// var video_link = $("#video_link").val();
-		// var video_lecture_date = $("#video_lecture_date").val();
-		// data = {
-		// 	name: name,
-		// 	description: description,
-		// 	video_link: video_link,
-		// 	video_lecture_date
-		// }
-
-	// });
-
 });
