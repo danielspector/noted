af1179b5084f1899195d882d643213f10edf3206
irmiller22 <irmiller22@gmail.com>
2013-12-04 21:56:19 -0500

    added duration to video 5 seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ videos = Video.create([
 	{name: "Lecture 2", instructor_id: 1, link: "http://flatiron-school.s3.amazonaws.com/ruby-003/reviews/unit-2-review-with-activerecord.mp4", description: "Ruby on Rails: ActiveRecord Review", lecture_date: "02/11/2013"},
 	{name: "Lecture 3", instructor_id: 1, link: "http://flatiron-school.s3.amazonaws.com/ruby-003/reviews/associations.and.forms.mp4", description: "Discussion of Associations and Forms", lecture_date: "02/11/2013"}, 
 	{name: "Lecture 4" , instructor_id: 1, link: "http://flatiron-school.s3.amazonaws.com/ruby-003/reviews/activerecord-associations.mp4", description: "ActiveRecord Associations", lecture_date: "03/11/2013"},
-	{name: "Lecture 5", instructor_id: 1, link: "http://flatiron-school.s3.amazonaws.com/ruby-003/reviews/a-tour-of-rails-with-dhh.mp4", description: "Rails with David H. Hansson", lecture_date: "03/11/2013"}, 
+	{name: "Lecture 5", instructor_id: 1, link: "http://flatiron-school.s3.amazonaws.com/ruby-003/reviews/a-tour-of-rails-with-dhh.mp4", description: "Rails with David H. Hansson", lecture_date: "03/11/2013", duration: "6415.333333"}, 
 	{name: "Lecture 6", instructor_id: 1, link: "http://flatiron-school.s3.amazonaws.com/ruby-003/reviews/rails-controller-tour.mp4", description: "Introduction to Rails Controller", lecture_date: "04/11/2013"}, 
 	{name: "Lecture 7", instructor_id: 1, link: "http://flatiron-school.s3.amazonaws.com/ruby-003/reviews/end-of-rails-scaffold-tour.mp4", description: "End of the Rails Scaffold Tour", lecture_date: "07/11/2013"},
 	{name: "Lecture 8", instructor_id: 1, link: "http://flatiron-school.s3.amazonaws.com/ruby-003/reviews/playister-associations-with-artists.mp4", description: "Playlister App - Associations with Artists", lecture_date: "10/11/2013"}, 
