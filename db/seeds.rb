6232b4fc71fd1bc6c77059990f49a901806f5b54
irmiller22 <irmiller22@gmail.com>
2013-12-12 13:42:49 -0500

    added new seed data for previous Users from production_db, deleted seed data with previous passwords

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,20 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-users = User.create([{name: "Avi Flombaum", email: "avi@flatironschool.com", password_digest: "itest1", permission_type: "instructor"},{name: "Stephanie Oh", email: "stephoh@gmail.com", password_digest: "stest1", permission_type: "student"}, {name: "Ian Miller", email: "irmiller22@gmail.com", password_digest: "stest22", permission_type: "student"}, {name: "Saron Yitbarek", email: "saron.yitbarek@gmail.com", password_digest: "stest1", permission_type: "student"}])
+users = User.create([
+  {name: "Avi Flombaum", email: "flombaum@flatironschool.com", password: "itest1", password_confirmation: "itest1", permission_type: "instructor"},
+  {name: "Stephanie Oh", email: "stephoh@gmail.com", password: "stest1", password_confirmation: "stest1", permission_type: "student"},
+  {name: "Ian Miller", email: "irmiller22@gmail.com", password: "stest22", password_confirmation: "stest22", permission_type: "student"},
+  {name: "Saron Yitbarek", email: "saron.yitbarek@gmail.com", password: "stest1", password_confirmation: "stest1", permission_type: "student"},
+  {name: "John", email: "jmclaughlin@shutterstock.com", password: "noted123", password_confirmation: "noted123", permission_type: "general"},
+  {name: "Raymond Gan", email: "rayning@yahoo.com", password: "password", password_confirmation: "password", permission_type: "general"},
+  {name: "Adam Jonas", email: "adam.jonas@carrotcreative.com", password: "carrot", password_confirmation: "carrot", permission_type: "general"},
+  {name: "Kyle MacDonald", email: "kyle@carrotcreative.com", password: "dontstea1", password_confirmation: "dontstea1", permission_type: "general"},
+  {name: "Taylor Smith", email: "twsmith89@gmail.com", password: "tarheel12", password_confirmation: "tarheel12", permission_type: "general"},
+  {name: "Adam Enbar", email: "adam@flatironschool.com", password: "ljljmmm", password_confirmation: "ljljmm", permission_type: "general"},
+  {name: "Avi Flombaum", email: "avi@flatironschool.com", password: "flomword3045G", password_confirmation: "flomword3045G", permission_type: "general"},
+  {name: "Annie Jung", email: "anniejung90@gmail.com", password: "ajrocks'", password_confirmation: "ajrocks'", permission_type: "general"}
+])
 
 # videos = Video.create([
 	# {name: "Lecture 1", user_id: 1, link: "http://flatiron-school.s3.amazonaws.com/ruby-003/reviews/railstour.mp4", description: "Overview for Ruby on Rails", lecture_date: "01/11/2013"}, 
