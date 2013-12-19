23610e10f4ececefd4669ef6255f3736f1a557d1
sarony <saron.yitbarek@gmail.com>
2013-12-18 23:51:29 -0500

    created video_description_on_skin.js

--- app/assets/javascripts/video_1.js
+++ app/assets/javascripts/video_1.js
@@ -5,24 +5,6 @@ $(document).ready(function(){
     $(".note_buttons").hide();
   });
 
-  // append video.name and video.description to control bar skin
-
-  var video_name = $(".video_name").val();
-  var video_description = $(".video_description").val();
-  
-  if(video_description == ""){
-    var description = ""
-  }
-  else {
-    var description = " - "+video_description
-  }
-
-  var label = '<h3 class="controlbar-video-name">'+video_name+description+'</h3>';
-  setTimeout(function(){
-    $(".vjs-duration").after(label);
-  }, 500); 
-
-
   // get width of timeline
   var timelineLength = $("#timeline").width()-100;
 
