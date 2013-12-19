2327c4780d71c9cac1698ffb5e147da5f9309177
sarony <saron.yitbarek@gmail.com>
2013-12-19 00:38:57 -0500

    created new_note.js, remove_md_preview.js, and refactoring video.js

--- app/assets/javascripts/video_1.js
+++ app/assets/javascripts/video_1.js
@@ -105,63 +105,6 @@ $(document).ready(function(){
   });
 
 
-
-  
-
-  // focus on textarea to pause video and create timestamp
-
-  $("#new-note .field textarea").focus(function(e){
-    e.preventDefault();
-
-    var $myPlayer = $("#vjs_video_4_html5_api");
-    // var $myPlayer = $("#lecture_video");
-    $myPlayer[0].pause();
-    var timeStamp = $myPlayer[0].currentTime;
-    console.log(timeStamp);
-    $("#new-note #note_video_timestamp").val(timeStamp);
-    // hide edit form, showing new note form and focusing textarea on "New Note" click
-    $(".edit_form").hide();  
-    $(".new_note_form, new_note").show();
-
-    // getting the video duration
-    var video_duration = $("#video_duration");
-    if(video_duration.val()==""){
-      video_duration.val($myPlayer[0].duration)     
-    }
-
-  });
-
-  // toggling create video form for instructor -- video.index
-  // $('.nav-new-video a').click(function(){
-  //   console.log("hi");
-  //   $(this).closest('.new_video').find('.video_form').toggle();
-
-  // });
-
-  // pause and show new note form on "new note" button
- $(".new_note").on("click", "button", function(e){
-    e.preventDefault();
-
-    var $myPlayer = $("#vjs_video_4_html5_api");
-    // var $myPlayer = $("#lecture_video");
-    $myPlayer[0].pause();
-    var timeStamp = $myPlayer[0].currentTime;
-    $("#new-note #note_video_timestamp").val(timeStamp);
-    // hide edit form, showing new note form and focusing textarea on "New Note" click
-    $(".edit_form").hide();  
-    $(".new_note_form, new_note").show();
-    $(".render-edit-form").html("<p></p>");
-    $(".field textarea").focus();
-
-    // getting the video duration
-    var video_duration = $("#video_duration");
-    if(video_duration.val()==""){
-      video_duration.val($myPlayer[0].duration)     
-    }
-
-  });
-
-
   // when "Create Note" is clicked, plays video, sends post with new note info, refreshes edit section 
   $("input[value='Create Note']").click(function(e){
     // $('.new_note_form').toggle();
@@ -184,19 +127,6 @@ $(document).ready(function(){
 
         // creating markers based on timestamp and with note.id anchor tags
         makeMarkers();
-        // var timelineLength = $("#timeline").width()-100;
-        // var duration = $("#video_duration").val();
-        // var idBucket = [];
-        // var $allNotes = $(".added_note");
-        // $allNotes.each(function(){
-        //   var noteId = $(this).find(".added_note_id").val();
-        //   idBucket.push(noteId);
-        // });
-        // var note_id = idBucket.sort().reverse()[0];
-        // var noteTimestamp = $("#note_wrapper_"+note_id+" .added_note_timestamp").val();
-        // var timeline = (((noteTimestamp/duration)*timelineLength)+50);
-        // var marker = '<a href="#'+note_id+'" class="marker" data-id="'+note_id+'" style="left:'+timeline+'px;"></a>';           
-        // $('.all_markers').append(marker);
 
         // hide edit form 
         $(".edit_form").hide(); 
@@ -216,88 +146,3 @@ $(document).ready(function(){
   removePreview();
 
 });
-
-
-// from js.fiddle -- resize window
-
-// $(window).resize(function(){
-//    var $video = $('#Box_Video');            $("#timeline").width($video[0].width());
-//     alert($video[0].width());
-// });
-
-// resizing the window with draggable
-
-// var div_orig = $(".divider").top-position;
-// $(".divider").draggable(function(){
-//   var div_drag = $(".divider").top-position;
-//   var notes_height = $("#notes").height;
-//   var div_pos_change = ((div_orig - div_drag)/notes_height)*100%
-//   var c1 = $("#note_all").height;
-//   var c2 = $("#new-note").height;
-//   c1.height(c1 - div_pos_change);
-//   c2.height(c2 + div_pos_change);
-// })
-
-
-// TRYING TO GET THE VIDEO META DATA DYNAMICALLY
-
-  // var myPlayer = $('#vjs_video_4_html5_api');
-  // (myPlayer).get(0).load(function(){
-  //   alert("Hi");
-  // });
-  
-  
-  // setTimeout(function(){
-  //     var $myPlayer = $("#vjs_video_4_html5_api");
-  // //     while($myPlayer[0].duration==undefined){
-  // //     setTimeout(function(){
-  // //       $myPlayer[0].duration;
-  // //     }, 500);
-  // // }
-
-  // alert($myPlayer[0].duration);
-  // }, 1000)
-
-
-  // setTimeout(function(){
-      // var $myPlayer = $("#vjs_video_4_html5_api");
-      // // $myPlayer.ready(function(){
-      //   $myPlayer.on("durationchange", function(){
-      //     alert($myPlayer[0].duration);
-      //   })
-        // $myPlayer.on('loadedmetadata', function(){
-        // alert($myPlayer[0].duration);
-       // })
-    
-    
-      // $myPlayer[0].onloadedmetadata = alert($myPlayer[0].duration);
-      // console.log($myPlayer[0].duration);
-    // }, 0);
-    
-    // var video = document.createElement('video')
-    // var $myPlayer = $("#vjs_video_4_html5_api");
-    // video.addEventListener("loadedmetadata", function(e){
-    //   // alert("Hi");
-    // })
-    // video.onloadedmetadata = alert("Hi");
-
-    // $myPlayer.on("loadeddata", function(){
-    //   alert("Hey now!")
-    // })
-
-//     var $myPlayer = $("#vjs_video_4_html5_api");
-
-      
-//       myVid=document.getElementById("vjs_video_4_html5_api");
-// myVid.onLoadedMetaData=alert($myPlayer.duration);
-
-    // setTimeout(function(){
-    //   var $myPlayer = $("#vjs_video_4_html5_api");
-    //   $myPlayer[0].play();
-    //   $myPlayer[0].addEventListener("loadedmetadata", function(e){
-    //     $myPlayer[0].duration
-    //   })
-    //   $myPlayer[0].loadedmetadata;
-    // }, 300);
-
-  // alert($("#vjs_video_4_html5_api")[0].duration);
