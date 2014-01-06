a3f98ceeee2d346b691bb972a524cabf85fb37e6
irmiller22 <irmiller22@gmail.com>
2014-01-06 14:48:39 -0500

    fixed issue with jquery code on sign_up partial

--- app/assets/javascripts/remove_md_preview.js
+++ app/assets/javascripts/remove_md_preview.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){ 
   function removePreview(){
-    $("#new-note .btn-group")[3].remove();
+    $("#new-note").find(".btn-group").eq(2).remove();
   }
   removePreview();
 });
+
+$('p') //<-- jquery object selection
+$('p')[0] //<-- DOM selection
